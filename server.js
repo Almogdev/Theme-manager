e45a0c0f4dfb1c7e0b3891f9768a9eb820410044
Almog <almogluzon@gmail.com>
2023-09-12 20:36:23 +0300

    added server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+app.use(express.json());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.set("view engine", "ejs");
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, "css")));
+//app.use(express.static(path.join(__dirname, "views")));
+
+var db_M = require('./database');
+global.db_pool = db_M.pool;
+
+const ThemeSite = require('./MainPage_R');
+app.use('/', ThemeSite);
+
+app.listen(port, () => {
+    console.log(`Now listening on port http://localhost:${port}`);
+});
